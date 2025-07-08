43f4bfed47e4d3a47324a91746c03849499ae058
diar.qoroviqi <diar.qoroviqi-ext@heyteam.com>
2025-07-08 15:48:39 +0200

    Modeling the app

---
+++ app/Models/Order.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\OrderStatus;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Order extends Model
+{
+    protected $fillable = ['customer_id', 'status'];
+
+    protected $casts = [
+        'status' => OrderStatus::class,
+    ];
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class)
+            ->withPivot('quantity')
+            ->withTimestamps();
+    }
+}
